18886842a0a5ddde6a8a5c6fa3af9ba855b6f038
immortals <panzhuowensky@gamil.com>
2018-02-15 21:21:52 +0800

    第三方中间件整合

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/auth':{
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+      '/api':{
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -20,7 +29,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
@@ -66,4 +75,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const Koa = require("koa");
+// import './env'
+import Koa from 'koa'
+// import json from "koa-json"
 
-const App = new Koa();
+const app = new Koa();
 
-App.use(async (ctx) => {
+app.use(async (ctx) => {
   ctx.body = 'hello koa2';
 });
 
-App.listen(3000, () => {
+app.listen(3000, () => {
   console.log('ump rest server start at port(s): 3000')
 });
+
+module.exports = app;
